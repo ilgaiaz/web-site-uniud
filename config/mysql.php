193ac334ea2b274818cbf30819444d787c2e739a
michele <michele.gaiarin@gmail.com>
2019-08-10 13:29:04 +0200

    back to old config with clearDB data

--- config/mysql.php
+++ config/mysql.php
@@ -1,16 +1,14 @@
 <?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  return "dbname=abcdefg host=heroku_1c6bc9375739b50 port=5432 user=b6df9263765a9a password=13c81934 sslmode=require";
+$servername = "eu-cdbr-west-02.cleardb.net";
+$username = "b6df9263765a9a";
+$password = "13c81934";
+$db = "13c81934";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
- 
-# Establish db connection
-$conn = pg_connect(pg_connection_string());
-if (!$conn) {
-    echo "Database connection error."
-    exit;
-}
- 
-$result = pg_query($conn, "SELECT statement goes here");
 ?>
