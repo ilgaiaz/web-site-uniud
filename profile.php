a2c5401c49286842c5b1c200059b458c6b701859
michele <michele.gaiarin@gmail.com>
2019-08-13 17:59:40 +0200

    Add personal page

---
+++ profile.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>Personal info</title>
+      
+      <meta name="description" content="Show profile data"/>
+      <meta name="author" content="Michele Gaiarin"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="icon" href="images/icon.png"/>
+      <link rel="stylesheet" type="text/css" href="style.css"/>
+      <script type="text/javascript" src="script.js"></script>
+      <script
+         src="https://code.jquery.com/jquery-3.4.0.js"
+         integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo="
+         crossorigin="anonymous">
+      </script>
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <!-- Popper JS -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+      <!-- Latest compiled JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+   </head>
+
+   <body>
+      <!--Navbar show -->
+      <div class="container" id="nav-placeholder">
+         <script>
+            $(function(){
+               $("#nav-placeholder").load("includes/navbar.php");
+            });
+         </script>
+      </div>
+
+      <div id="container-resetpw" class=" container">
+            <div class="jumbotron">
+                <div class="form-container">
+                    <div class="col-sm">
+                        <?php
+                            session_start();
+                            require_once('config/mysql.php');
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+      <!-- End Navbar -->
+    </body>
+</html>
--- services/get.php
+++ services/get.php
@@ -1,15 +1,21 @@
 <table border="1">
     <?php
-        require_once('../config/mysql.php');
-        $query='SELECT * FROM user_data';
-        $result = $conn->query($query);
-        if(!$result) {
+        session_start();
+        require_once('config/mysql.php');
+        $query="SELECT * FROM user_data WHERE user = '".$_SESSION["username"]."';";
+        $result = mysqli_query($conn,$query);
+        $data = mysqli_fetch_assoc($query);
+        if(!$result->num_rows > 0) {
             echo "Error";
-        }
-        //Loop and create table 
-        while ($data = $result->fetch_array()) {
-            // var_dump($data);
+        } else {
+            echo "<table><tr><th>Username</th><th>Nome</th><th>Cognome</th><th>Data di nascita</th><th>Emal</th></tr>";
             echo "<tr><td>".$data['User']."</td></tr>";
+            //Loop and create table 
+            while ($row = $result->fetch_assoc()) {
+                // var_dump($data);
+                echo "<tr><td>".$data['user']."</td><td>".$data['name']."</td>
+                <td>".$data['surname']."</td><td>".$data['dateOfBrith']."</td><td>".$data['email']."</td></tr>";
+            }
         }
     ?>
 </table>
