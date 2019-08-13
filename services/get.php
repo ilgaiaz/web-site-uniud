1a53a0f24b95d4c0d48e89b903145449cea78d2e
michele <michele.gaiarin@gmail.com>
2019-08-13 22:52:03 +0200

    Fix table with personal data

--- services/get.php
+++ services/get.php
@@ -1,17 +1,36 @@
-<table border="1">
-    <?php
-        session_start();
-        require_once('config/mysql.php');
-        $query="SELECT * FROM user_data WHERE user = '".$_SESSION["username"]."';";
-        $result = mysqli_query($conn,$query);
-        $data = mysqli_fetch_assoc($query);
-        $row = mysqli_num_rows($results);
-        if($row=="") {
-            echo "Error: ".$_SESSION["username"];
-        } else {
-            echo "<table><tr><th>Username</th><th>Nome</th><th>Cognome</th><th>Data di nascita</th><th>Emal</th></tr>";
-            echo "<tr><td>".$data['user']."</td><td>".$data['name']."</td>
-                <td>".$data['surname']."</td><td>".$data['dateOfBrith']."</td><td>".$data['email']."</td></tr>";
-        }
-    ?>
-</table>
+<div>
+    <table class="table table-striped table-hover">
+        <?php
+            session_start();
+            require_once('config/mysql.php');
+            $query="SELECT * FROM user_data WHERE user = '".$_SESSION["username"]."';";
+            $result = mysqli_query($conn,$query);
+            $data = mysqli_fetch_assoc($result);
+            $row = mysqli_num_rows($result);
+            if($row=="") {
+                echo "Error";
+            } else {
+            ?>
+                <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Nome</th>
+                    <th>Cognome</th>
+                    <th>Data di nascita</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?=$data['user'];?></td>
+                        <td><?=$data['name'];?></td>
+                        <td><?=$data['surname'];?></td>
+                        <td><?=$data['dateOfBirth'];?></td>
+                        <td><?=$data['email'];?></td>
+                    </tr>
+                    </tbody>
+            <?php
+            }
+        ?>
+    </table>
+</div>
