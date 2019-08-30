22593e8c7bc5254a12c269860b9801fb3163da45
michele <michele.gaiarin@gmail.com>
2019-08-30 12:12:05 +0200

    Add GROUP BY sql + fix encoding
    Fix description encoding with utf8

--- products.php
+++ products.php
@@ -84,7 +84,7 @@
 										<tr>
 											<td class="prod-img"><img class="img-project" src="<?php echo $path;?>"></td>
 											<td class="prod-spec-name"><?php echo $name;?></td>
-											<td class="prod-spec-description"><?php echo $descr;?></td>
+											<td class="prod-spec-description"><?php echo utf8_encode($descr);?></td>
 											<td class="prod-spec-power" nowrap><?php echo $pow ;?> W</td>
 											<td class="prod-spec-cost" nowrap>€ <?php echo $cost ;?></td>
 											<td class="prod-spec-link" nowrap>
