2d55167ddf65a0da9832ad13202f6b74c3096339
michele <michele.gaiarin@gmail.com>
2019-08-30 16:15:57 +0200

    Add icon and page's name

--- projects.php
+++ projects.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Progetti</title>
+		<title>Componenti</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="icon" href="images/icon.png">
 		<script
